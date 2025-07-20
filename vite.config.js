f4aa555b86a2b5bea7f030949d39aff6bf065a11
Rehan Mokashi <165246963+rmokashi01@users.noreply.github.com>
2025-07-20 22:04:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/rehan-portfolio/', // 👈 Must match your repo name
   plugins: [react()],
 })
